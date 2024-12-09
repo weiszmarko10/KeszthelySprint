522ea6bf7821e114362f1097fc94c455f7d1860d
weiszmarko10 <weiszm.22d@acsjszki.hu>
2024-12-09 14:01:45 +0100

    Add project files.

---
+++ KeszthelySprint/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace KeszthelySprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> versenyzok = new();
+            foreach (var sor in File.ReadAllLines("eredmenyek.txt"))
+            {
+                versenyzok.Add(new Versenyzo(sor));
+            }
+            Console.WriteLine($"{versenyzok.Count} ");
+
+            var junior = 0;
+
+        }
+    }
+}
---
+++ KeszthelySprint/Versenyzo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeszthelySprint
+{
+    internal class Versenyzo
+    {
+        public string Név { get; set; }
+        public int SzületésiÉv { get; set; }
+        public int Rajtszám { get; set; }
+        public string Nem { get; set; }
+        public string ÉletkorKategória { get; set; }
+        public string ÚszásIdeje { get; set; }
+        public string DepóbanTöltöttIdő { get; set; }
+        public string KerékPározásIdeje { get; set; }
+        public string MásodikDepóIdő { get; set; }
+        public string FutásIdeje { get; set; }
+
+        public Versenyzo(string sor)
+        {
+            string[] s = sor.Split(';');
+
+
+            Név = s[0];
+            SzületésiÉv = int.Parse(s[1]);
+            Rajtszám = int.Parse(s[2]);
+            Nem = s[3];
+            ÉletkorKategória = s[4];
+            ÚszásIdeje = s[5];
+            DepóbanTöltöttIdő = s[6];
+            KerékPározásIdeje = s[7];
+            MásodikDepóIdő = s[8];
+            FutásIdeje = s[9];
+
+        }
+        
+        
+             
+
+
+        
+    }
+}
